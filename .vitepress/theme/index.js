c95e7bd33a1eaeacbd45ee83dee8d40a397f5a77
guobin <guobin@xaidc.com>
2026-01-19 11:22:30 +0800

    fix: 重构仓库

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,5 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
+
 import 'element-plus/dist/index.css'
+import './custom.css'
 import ElementPlus from "element-plus"
 const CustomTheme = {
   ...DefaultTheme,
